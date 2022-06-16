66a2175037c63af6097731406aa95ad2909cc2d5
LeeAlephium <lee@alephium.org>
2022-06-16 09:04:27 -0400

    Migrate from walletconnect beta.26 to beta.101

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,6 @@
 import { EventEmitter } from "eventemitter3";
-import { JsonRpcProvider } from "@walletconnect/jsonrpc-provider";
-import { RequestArguments } from "@walletconnect/jsonrpc-utils";
-import { SessionTypes } from "@walletconnect/types";
-import {
-  SignerConnection,
-  SIGNER_EVENTS,
-  SignerConnectionClientOpts,
-} from "@walletconnect/signer-connection";
+import SignClient, { SIGN_CLIENT_EVENTS } from "@walletconnect/sign-client";
+import { ISignClient, SignClientTypes } from "@walletconnect/types";
 import {
   SignerProvider,
   Account,
@@ -26,18 +20,26 @@ import {
   groupOfAddress,
 } from "@alephium/web3";
 
-// Note:
-// 1. the wallet client could potentially submit the signed transaction.
-// 2. `alph_signUnsignedTx` can be used for complicated transactions (e.g. multisig).
+// Only available in TypeScript 4.5+
+// Thank you Gerrit0: https://stackoverflow.com/a/49889856
+type Awaited<T> = T extends PromiseLike<infer U>
+  ? { 0: Awaited<U>; 1: U }[U extends PromiseLike<any> ? 0 : 1]
+  : T
+
 export const signerMethods = [
   "alph_getAccounts",
+
+  // Can also submit the signed transaction via the connected wallet.
   "alph_signTransferTx",
   "alph_signContractCreationTx",
   "alph_signScriptTx",
+
+  // Can be used for complicated transactions (e.g. multisig).
   "alph_signUnsignedTx",
   "alph_signHexString",
   "alph_signMessage",
 ];
+
 type SignerMethodsTuple = typeof signerMethods;
 type SignerMethods = SignerMethodsTuple[number];
 
@@ -71,34 +73,26 @@ interface SignerMethodsTable extends Record<SignerMethods, { params: any; result
     result: SignMessageResult;
   };
 }
+
 export type MethodParams<T extends SignerMethods> = SignerMethodsTable[T]["params"];
 export type MethodResult<T extends SignerMethods> = SignerMethodsTable[T]["result"];
 
-export const providerEvents = {
-  changed: {
-    network: "networkChanged",
-    accounts: "accountsChanged",
-  },
-};
+export type SessionMetadata = Awaited<ReturnType<ISignClient["approve"]>>;
 
 export interface WalletConnectProviderOptions {
   networkId: number;
   chainGroup: number;
-  methods?: string[];
-  client?: SignerConnectionClientOpts;
+  client: SignClient;
 }
 
 class WalletConnectProvider implements SignerProvider {
-  public events: any = new EventEmitter();
-
   public static namespace = "alephium";
   public networkId: number;
   public chainGroup?: number;
-  public methods = signerMethods;
+  public meta?: SessionMetadata;
+  public session: SignClient;
 
-  public accounts: Account[] = [];
-
-  public signer: JsonRpcProvider;
+  private methods = signerMethods;
 
   get permittedChain(): string {
     return formatChain(this.networkId, this.chainGroup);
@@ -107,75 +101,14 @@ class WalletConnectProvider implements SignerProvider {
   constructor(opts: WalletConnectProviderOptions) {
     this.networkId = opts.networkId;
     this.chainGroup = opts.chainGroup;
-    this.methods = opts.methods ? [...opts.methods, ...this.methods] : this.methods;
-    this.signer = this.setSignerProvider(opts.client);
-    this.registerEventListeners();
-  }
-
-  // The provider only supports signer methods. The other requests should use Alephium Rest API.
-  public async request<T = unknown>(args: RequestArguments): Promise<T> {
-    if (args.method === "alph_getAccounts") {
-      return this.accounts as any;
-    }
-    if (this.methods.includes(args.method)) {
-      const signerAddress = args.params?.signerAddress;
-      if (typeof signerAddress === "undefined") {
-        throw new Error("Cannot request without signerAddress");
-      }
-      const signerAccount = this.accounts.find(
-        account => account.address === args.params.signerAddress,
-      );
-      if (typeof signerAccount === "undefined") {
-        throw new Error(`Unknown signer address ${args.params.signerAddress}`);
-      }
-      return this.signer.request(args, {
-        chainId: formatChain(this.networkId, this.chainGroup),
-      });
-    }
-    return Promise.reject(`Invalid method was passed ${args.method}`);
-  }
-
-  public async connect(): Promise<GetAccountsResult> {
-    await this.signer.connect();
-    return this.accounts;
-  }
-
-  get connected(): boolean {
-    return (this.signer.connection as SignerConnection).connected;
-  }
-
-  get connecting(): boolean {
-    return (this.signer.connection as SignerConnection).connecting;
-  }
-
-  public async disconnect(): Promise<void> {
-    await this.signer.disconnect();
-  }
-
-  public on(event: any, listener: any): void {
-    this.events.on(event, listener);
-  }
-  public once(event: string, listener: any): void {
-    this.events.once(event, listener);
-  }
-  public removeListener(event: string, listener: any): void {
-    this.events.removeListener(event, listener);
-  }
-  public off(event: string, listener: any): void {
-    this.events.off(event, listener);
-  }
-
-  get isWalletConnect() {
-    return true;
+    this.session = opts.client;
+    this.meta = undefined;
   }
 
   // ---------- Methods ----------------------------------------------- //
 
-  private typedRequest<T extends SignerMethods>(
-    method: T,
-    params: MethodParams<T>,
-  ): Promise<MethodResult<T>> {
-    return this.request({ method: method, params: params });
+  public setSessionMetadata(meta: SessionMetadata) {
+    this.meta = meta;
   }
 
   public getAccounts(): Promise<Account[]> {
@@ -212,98 +145,25 @@ class WalletConnectProvider implements SignerProvider {
 
   // ---------- Private ----------------------------------------------- //
 
-  private registerEventListeners() {
-    this.signer.on("connect", async () => {
-      const chains = (this.signer.connection as SignerConnection).chains;
-      if (chains && chains.length) this.setChain(chains);
-      const accounts = (this.signer.connection as SignerConnection).accounts;
-      if (accounts && accounts.length) this.setAccounts(accounts);
-    });
-    this.signer.connection.on(SIGNER_EVENTS.created, (session: SessionTypes.Settled) => {
-      this.setChain(session.permissions.blockchain.chains);
-      this.setAccounts(session.state.accounts);
-    });
-    this.signer.connection.on(SIGNER_EVENTS.updated, (session: SessionTypes.Settled) => {
-      const chain = formatChain(this.networkId, this.chainGroup);
-      if (!session.permissions.blockchain.chains.includes(chain)) {
-        this.setChain(session.permissions.blockchain.chains);
-      }
-      this.setAccounts(session.state.accounts);
-    });
-    this.signer.connection.on(
-      SIGNER_EVENTS.notification,
-      (notification: SessionTypes.Notification) => {
-        if (notification.type === providerEvents.changed.accounts) {
-          this.setAccounts(notification.data);
-        } else if (notification.type === providerEvents.changed.network) {
-          this.networkId = notification.data;
-          this.events.emit(providerEvents.changed.network, this.networkId);
-        } else {
-          this.events.emit(notification.type, notification.data);
-        }
-      },
-    );
-    this.signer.on("disconnect", () => {
-      this.events.emit("disconnect");
-    });
-  }
-
-  private setSignerProvider(client?: SignerConnectionClientOpts) {
-    const connection = new SignerConnection({
-      chains: [formatChain(this.networkId, this.chainGroup)],
-      methods: this.methods,
-      client,
-    });
-    return new JsonRpcProvider(connection);
-  }
-
-  private sameChains(chains0: string[], chains1?: string[]): boolean {
-    if (typeof chains1 === "undefined") {
-      return false;
-    } else {
-      return chains0.join() === chains1.join();
-    }
-  }
-
-  private lastSetChains?: string[];
-  private setChain(chains: string[]) {
-    if (this.sameChains(chains, this.lastSetChains)) {
-      return;
-    } else {
-      this.lastSetChains = chains;
+  private typedRequest<T extends SignerMethods>(
+    method: T,
+    params: MethodParams<T>,
+  ): Promise<MethodResult<T>> {
+    if (!this.meta) {
+      return Promise.reject("Session has not be initialized yet. Must call provider.session.connect(...) first.");
     }
 
-    const compatible = chains.filter(x => isCompatibleChain(x));
-    if (compatible.length) {
-      [this.networkId, this.chainGroup] = parseChain(compatible[0]);
-      this.events.emit(providerEvents.changed.network, this.networkId);
+    if (this.methods.includes(method)) {
+      return this.session.request({
+        topic: this.meta.topic,
+        chainId: this.permittedChain,
+        request: {
+          method,
+          params,
+        },
+      });
     }
-  }
-
-  private sameAccounts(account0: Account[], account1?: Account[]): boolean {
-    if (typeof account1 === "undefined") {
-      return false;
-    } else {
-      return account0.map(a => a.address).join() === account1.map(a => a.address).join();
-    }
-  }
-
-  private lastSetAccounts?: Account[];
-  private setAccounts(accounts: string[]) {
-    const parsedAccounts = accounts.map(parseAccount);
-    if (this.sameAccounts(parsedAccounts, this.lastSetAccounts)) {
-      return;
-    } else {
-      this.lastSetAccounts = parsedAccounts;
-    }
-
-    const newAccounts = parsedAccounts.filter(account =>
-      isCompatibleChainGroup(account.group, this.chainGroup),
-    );
-    if (!this.sameAccounts(newAccounts, this.accounts)) {
-      this.accounts = newAccounts;
-      this.events.emit(providerEvents.changed.accounts, newAccounts);
-    }
+    return Promise.reject(`Invalid method was passed ${method}`);
   }
 }
 
@@ -337,6 +197,22 @@ export function parseAccount(account: string): Account {
     publicKey: publicKey,
     group: groupOfAddress(address),
   };
+}
+
+export function sameChains(chains0: string[], chains1?: string[]): boolean {
+  if (typeof chains1 === "undefined") {
+    return false;
+  } else {
+    return chains0.join() === chains1.join();
+  }
+}
+
+export function sameAccounts(account0: Account[], account1?: Account[]): boolean {
+  if (typeof account1 === "undefined") {
+    return false;
+  } else {
+    return account0.map(a => a.address).join() === account1.map(a => a.address).join();
+  }
 }
 
 export default WalletConnectProvider;
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,58 +1,19 @@
+import SignClient from '@walletconnect/sign-client';
 import "mocha";
 import { expect } from "chai";
 
-import WalletConnectProvider, {
-  formatChain,
-  isCompatibleChainGroup,
-  parseChain,
-} from "../src/index";
-import { WalletClient } from "./shared";
-import {
-  groupOfAddress,
-  node,
-  Account,
-  NodeProvider,
-  Contract,
-  Script,
-  verifyHexString,
-  verifySignedMessage,
-} from "@alephium/web3";
-import { PrivateKeyWallet } from "@alephium/web3/test";
+import WalletConnectProvider, { signerMethods, SessionMetadata } from "../src/index";
 
 const NETWORK_ID = 4;
 const CHAIN_GROUP = 2;
-const PORT = 22973;
-const RPC_URL = `http://alephium:${PORT}`;
 
-const nodeProvider = new NodeProvider(RPC_URL);
-const signerA = new PrivateKeyWallet(
-  nodeProvider,
-  "a642942e67258589cd2b1822c631506632db5a12aabcf413604e785300d762a5",
-);
-const signerB = PrivateKeyWallet.Random(nodeProvider);
-const signerC = PrivateKeyWallet.Random(nodeProvider);
-const ACCOUNTS = {
-  a: {
-    address: "1DrDyTr9RpRsQnDnXo2YRiPzPW4ooHX5LLoqXrqfMrpQH",
-    privateKey: signerA.privateKey,
-    group: signerA.group,
-  },
-  b: {
-    address: signerB.address,
-    privateKey: signerB.privateKey,
-    group: signerB.group,
-  },
-  c: {
-    address: signerC.address,
-    privateKey: signerC.privateKey,
-    group: signerC.group,
-  },
-};
-const ONE_ALPH = "1000000000000000000";
+const TEST_PROJECT_ID = process.env.TEST_PROJECT_ID
+  ? process.env.TEST_PROJECT_ID
+  : "6e2562e43678dd68a9070a62b6d52207";
 
 const TEST_RELAY_URL = process.env.TEST_RELAY_URL
   ? process.env.TEST_RELAY_URL
-  : "ws://localhost:5555";
+  : "wss://relay.walletconnect.com";
 
 const TEST_APP_METADATA = {
   name: "Test App",
@@ -70,371 +31,108 @@ const TEST_WALLET_METADATA = {
 
 const TEST_PROVIDER_OPTS = {
   networkId: NETWORK_ID,
-  chainGroup: CHAIN_GROUP,
-  rpc: {
-    custom: {
-      [NETWORK_ID]: RPC_URL,
-    },
-  },
-  client: {
-    relayUrl: TEST_RELAY_URL,
-    metadata: TEST_APP_METADATA,
-  },
+  chainGroup: CHAIN_GROUP
 };
 
 const TEST_WALLET_CLIENT_OPTS = {
-  networkId: NETWORK_ID,
-  rpcUrl: RPC_URL,
-  privateKey: ACCOUNTS.a.privateKey,
   relayUrl: TEST_RELAY_URL,
-  metadata: TEST_WALLET_METADATA,
-  submitTx: true,
+  projectId: TEST_PROJECT_ID,
+  metadata: TEST_WALLET_METADATA
 };
 
-describe("Unit tests", function() {
-  const expectedChainGroup0 = 2;
-  const expectedChainGroup1 = undefined;
-
-  it("test util functions", () => {
-    expect(formatChain(4, expectedChainGroup0)).to.eql("alephium:4/2");
-    expect(formatChain(4, expectedChainGroup1)).to.eql("alephium:4/-1");
-    expect(isCompatibleChainGroup(2, expectedChainGroup0)).to.eql(true);
-    expect(isCompatibleChainGroup(1, expectedChainGroup0)).to.eql(false);
-    expect(isCompatibleChainGroup(2, expectedChainGroup1)).to.eql(true);
-    expect(isCompatibleChainGroup(1, expectedChainGroup1)).to.eql(true);
-    expect(parseChain("alephium:4/2")).to.eql([4, 2]);
-    expect(parseChain("alephium:4/-1")).to.eql([4, undefined]);
-  });
-});
+const TEST_DAPP_OPTS = {
+  relayUrl: TEST_RELAY_URL,
+  projectId: TEST_PROJECT_ID,
+  metadata: TEST_APP_METADATA
+};
 
 describe("WalletConnectProvider with single chainGroup", function() {
   this.timeout(30_000);
 
-  let provider: WalletConnectProvider;
-  let walletClient: WalletClient;
+  let signClientWallet: SignClient;
+  let providerForDapp: WalletConnectProvider;
   let walletAddress: string;
+  let metaWallet: any = undefined;
+  let metaDapp: any = undefined;
+
   before(async () => {
-    provider = new WalletConnectProvider({
+    const signClientDapp = await SignClient.init(TEST_DAPP_OPTS);
+    const signClientWallet = await SignClient.init(TEST_WALLET_CLIENT_OPTS);
+
+    signClientWallet.on('session_proposal', async (proposal) => {
+      await signClientWallet.approve({
+        id: proposal.id,
+        namespaces: {
+          alephium: {
+            accounts: [],
+            methods: ['alph_signContractCreationTx', 'alph_signScriptTx', 'alph_signTransferTx', 'alph_getAccounts'],
+            events: []
+          }
+        }
+      })
+    });
+
+    providerForDapp = new WalletConnectProvider({
       ...TEST_PROVIDER_OPTS,
-      chainGroup: groupOfAddress(ACCOUNTS.a.address),
+      chainGroup: CHAIN_GROUP,
+      client: signClientDapp
     });
-    walletClient = await WalletClient.init(provider, TEST_WALLET_CLIENT_OPTS);
-    walletAddress = walletClient.signer.address;
-    expect(walletAddress).to.eql(ACCOUNTS.a.address);
-    const providerAccounts = await provider.connect();
-    expect(providerAccounts.map(a => a.address)).to.eql([walletAddress]);
-  });
-  after(async () => {
-    // disconnect provider
-    await Promise.all([
-      new Promise<void>(async resolve => {
-        provider.on("disconnect", () => {
-          resolve();
-        });
-      }),
-      new Promise<void>(async resolve => {
-        await walletClient.disconnect();
-        resolve();
-      }),
-    ]);
-    // expect provider to be disconnected
-    expect(walletClient.client?.session.values.length).to.eql(0);
-    expect(provider.connected).to.be.false;
-  });
-  it("networkChanged", async () => {
-    // change to testnet
-    await Promise.all([
-      new Promise<void>((resolve, reject) => {
-        provider.on("networkChanged", chainId => {
-          try {
-            expect(chainId).to.eql(1);
-            expect(provider.networkId).to.eql(1);
-            resolve();
-          } catch (e) {
-            reject(e);
-          }
-        });
-      }),
 
-      new Promise<void>(async (resolve, reject) => {
-        try {
-          await walletClient.changeChain(1, "https://testnet-wallet.alephium.org");
-          resolve();
-        } catch (e) {
-          reject(e);
+    const { uri, approval } = await providerForDapp.session.connect({
+      requiredNamespaces: {
+        alephium: {
+          chains: [providerForDapp.permittedChain],
+          methods: signerMethods,
+          events: []
         }
-      }),
-    ]);
-    // change back to devnet
-    await Promise.all([
-      new Promise<void>((resolve, reject) => {
-        provider.on("networkChanged", chain => {
-          try {
-            expect(chain).to.eql(NETWORK_ID);
-            resolve();
-          } catch (e) {
-            reject(e);
-          }
-        });
-      }),
-
-      new Promise<void>(async (resolve, reject) => {
-        try {
-          await walletClient.changeChain(NETWORK_ID, RPC_URL);
-          resolve();
-        } catch (e) {
-          reject(e);
-        }
-      }),
-    ]);
-  });
-  it("accountsChanged", async () => {
-    const changes: Account[][] = [];
-    provider.on("accountsChanged", accounts => {
-      changes.push(accounts);
+      }
     });
-    // change to account c
-    await Promise.all([
-      new Promise<void>((resolve, reject) => {
-        provider.on("accountsChanged", accounts => {
-          try {
-            if (ACCOUNTS.c.group == ACCOUNTS.a.group) {
-              expect(accounts[0].address).to.eql(ACCOUNTS.c.address);
-            } else {
-              expect(accounts).to.eql([]);
-            }
-            resolve();
-          } catch (e) {
-            reject(e);
-          }
-        });
-      }),
 
-      new Promise<void>(async (resolve, reject) => {
-        try {
-          await walletClient.changeAccount(ACCOUNTS.c.privateKey);
-
-          resolve();
-        } catch (e) {
-          reject(e);
-        }
-      }),
-    ]);
-    // change back to account a
-    await Promise.all([
-      new Promise<void>((resolve, reject) => {
-        provider.on("accountsChanged", accounts => {
-          try {
-            expect(accounts[0].address).to.eql(ACCOUNTS.a.address);
-            resolve();
-          } catch (e) {
-            reject(e);
-          }
-        });
-      }),
-      new Promise<void>(async (resolve, reject) => {
-        try {
-          await walletClient.changeAccount(ACCOUNTS.a.privateKey);
-          resolve();
-        } catch (e) {
-          reject(e);
-        }
-      }),
-    ]);
-  });
-
-  function delay(ms: number) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
-
-  let balance: node.Balance;
-  async function checkBalanceDecreasing() {
-    delay(500);
-    const balance1 = await nodeProvider.addresses.getAddressesAddressBalance(ACCOUNTS.a.address);
-    expect(balance1.utxoNum).to.eql(1);
-    if (balance1.balance >= balance.balance) {
-      checkBalanceDecreasing();
+    if (uri) {
+      await signClientWallet.pair({ uri });
     }
-    balance = balance1;
-  }
 
-  it("should sign", async () => {
-    const accounts = await provider.getAccounts();
-    expect(!!accounts).to.be.true;
-    expect(accounts[0].address).to.eql(ACCOUNTS.a.address);
-
-    balance = await nodeProvider.addresses.getAddressesAddressBalance(ACCOUNTS.a.address);
-    expect(balance.utxoNum).to.eql(1);
-
-    expect(walletClient.submitTx).to.be.true;
-
-    await provider.signTransferTx({
-      signerAddress: signerA.address,
-      destinations: [{ address: ACCOUNTS.b.address, attoAlphAmount: ONE_ALPH }],
-    });
-    await checkBalanceDecreasing();
-
-    const greeter = await Contract.fromSource(nodeProvider, "greeter.ral");
-
-    const greeterParams = await greeter.paramsForDeployment({
-      signerAddress: signerA.address,
-      initialFields: { btcPrice: 1 },
-    });
-    const greeterResult = await signerA.signDeployContractTx(greeterParams);
-    await checkBalanceDecreasing();
-
-    const main = await Script.fromSource(nodeProvider, "greeter_main.ral");
-    const mainParams = await main.paramsForDeployment({
-      signerAddress: signerA.address,
-      initialFields: { greeterContractId: greeterResult.contractId },
-    });
-    const mainResult = await signerA.signExecuteScriptTx(mainParams);
-    await checkBalanceDecreasing();
-
-    const hexString = "48656c6c6f20416c65706869756d21";
-    const signedHexString = await signerA.signHexString({
-      hexString: hexString,
-      signerAddress: signerA.address,
-    });
-    const message = "Hello Alephium!";
-    const signedMessage = await signerA.signMessage({
-      message: message,
-      signerAddress: signerA.address,
-    });
-    expect(signedMessage.signature).not.to.eql(signedHexString.signature);
-    expect(verifyHexString(hexString, signerA.publicKey, signedHexString.signature)).to.be.true;
-    expect(verifySignedMessage(message, signerA.publicKey, signedMessage.signature)).to.be.true;
+    metaWallet = await approval();
   });
-});
 
-describe("WalletConnectProvider with arbitrary chainGroup", function() {
-  this.timeout(30_000);
-
-  let provider: WalletConnectProvider;
-  let walletClient: WalletClient;
-  let walletAddress: string;
-  before(async () => {
-    const { chainGroup, ...providerOpts } = TEST_PROVIDER_OPTS;
-    provider = new WalletConnectProvider({ chainGroup: -1, ...providerOpts });
-    walletClient = await WalletClient.init(provider, TEST_WALLET_CLIENT_OPTS);
-    walletAddress = walletClient.signer.address;
-    expect(walletAddress).to.eql(ACCOUNTS.a.address);
-    const providerAccounts = await provider.connect();
-    expect(providerAccounts.map(a => a.address)).to.eql([walletAddress]);
-  });
   after(async () => {
-    // disconnect provider
     await Promise.all([
       new Promise<void>(async resolve => {
-        provider.on("disconnect", () => {
+        providerForDapp.session.on("session_delete", () => {
+          console.log('session deleted for dapp');
           resolve();
         });
+        providerForDapp.session.on("pairing_delete", (event) => {
+          console.log('pairing deleted for dapp');
+          resolve();
+        });
+        await providerForDapp.session.disconnect({
+          topic: metaDapp.topic,
+          reason: { code: 1, message: "testing complete" }
+        });
+        console.log('disconnected dapp');
       }),
       new Promise<void>(async resolve => {
-        await walletClient.disconnect();
+        signClientWallet.on("session_delete", () => {
+          console.log('session deleted for wallet');
+          resolve();
+        });
+        signClientWallet.on("pairing_delete", (event) => {
+          console.log('pairing deleted for wallet');
+          resolve();
+        });
+        await signClientWallet.disconnect({
+          topic: metaWallet.topic,
+          reason: { code: 1, message: "testing complete" }
+        });
+        console.log('disconnected wallet');
         resolve();
       }),
     ]);
-    // expect provider to be disconnected
-    expect(walletClient.client?.session.values.length).to.eql(0);
-    expect(provider.connected).to.be.false;
-  });
-  it("networkChanged", async () => {
-    // change to testnet
-    await Promise.all([
-      new Promise<void>((resolve, reject) => {
-        provider.on("networkChanged", chainId => {
-          try {
-            expect(chainId).to.eql(1);
-            resolve();
-          } catch (e) {
-            reject(e);
-          }
-        });
-      }),
-
-      new Promise<void>(async (resolve, reject) => {
-        try {
-          await walletClient.changeChain(1, "https://testnet-wallet.alephium.org");
-          resolve();
-        } catch (e) {
-          reject(e);
-        }
-      }),
-    ]);
-    // change back to devnet
-    await Promise.all([
-      new Promise<void>((resolve, reject) => {
-        provider.on("networkChanged", chain => {
-          try {
-            expect(chain).to.eql(NETWORK_ID);
-            resolve();
-          } catch (e) {
-            reject(e);
-          }
-        });
-      }),
-
-      new Promise<void>(async (resolve, reject) => {
-        try {
-          await walletClient.changeChain(NETWORK_ID, RPC_URL);
-          resolve();
-        } catch (e) {
-          reject(e);
-        }
-      }),
-    ]);
   });
 
-  it("accountsChanged", async () => {
-    const changes: Account[][] = [];
-    provider.on("accountsChanged", accounts => {
-      changes.push(accounts);
-    });
-    // change to account c
-    await Promise.all([
-      new Promise<void>(async (resolve, reject) => {
-        try {
-          await walletClient.changeAccount(ACCOUNTS.c.privateKey);
-
-          resolve();
-        } catch (e) {
-          reject(e);
-        }
-      }),
-
-      new Promise<void>((resolve, reject) => {
-        provider.on("accountsChanged", accounts => {
-          try {
-            expect(accounts[0].address).to.eql(ACCOUNTS.c.address);
-            resolve();
-          } catch (e) {
-            reject(e);
-          }
-        });
-      }),
-    ]);
-    // change back to account a
-    await Promise.all([
-      new Promise<void>(async (resolve, reject) => {
-        try {
-          await walletClient.changeAccount(ACCOUNTS.a.privateKey);
-          resolve();
-        } catch (e) {
-          reject(e);
-        }
-      }),
-
-      new Promise<void>((resolve, reject) => {
-        provider.on("accountsChanged", accounts => {
-          try {
-            expect(accounts[0].address).to.eql(ACCOUNTS.a.address);
-            resolve();
-          } catch (e) {
-            reject(e);
-          }
-        });
-      }),
-    ]);
+  it('is happy', () => {
+    console.log(providerForDapp);
+    console.log(signClientWallet);
   });
 });
